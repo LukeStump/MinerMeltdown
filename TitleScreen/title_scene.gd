14597028d4fa6b70251dbe3c72a635aa3f2cac32
LukeStump <74683887+LukeStump@users.noreply.github.com>
2025-02-01 21:12:14 -0500

    Made ice melting opaque

--- TitleScreen/title_scene.gd
+++ TitleScreen/title_scene.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+@onready var HorseLogo = get_child(2)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	HorseLogo.play()
 	pass # Replace with function body.
 
 
